a2b04f5da8e76394e065c69a86d4e4b6fc144231
Sharvari Raut <sharuraut7blog@gmail.com>
2024-09-03 20:18:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
-load_dotenv() ##load all the nevironment variables
+#load_dotenv() ##load all the nevironment variables
 import os
 import google.generativeai as genai
 
