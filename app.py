a1217c1cc3dc49fc51be1552a44d692c5163f332
Sharvari Raut <sharuraut7blog@gmail.com>
2024-09-03 17:30:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,43 +1,43 @@
+import os
 import streamlit as st
 from dotenv import load_dotenv
+load_dotenv()
 
-load_dotenv() ##load all the nevironment variables
-import os
 import google.generativeai as genai
-
 from youtube_transcript_api import YouTubeTranscriptApi
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-prompt="""You are Youtube video summarizer. You will be taking the transcript text
-and summarizing the entire video and providing the important summary in points
-within 250 words. Please provide the summary of the text given here:  """
+
+prompt = """You are on Youtube video summarizer. 
+You will be taking the transcript text and summarizing 
+the entire video and provide the important summary in points within 250 words. 
+Please provide the summary of the text given here:  """
 
 
 ## getting the transcript data from yt videos
 def extract_transcript_details(youtube_video_url):
     try:
-        video_id=youtube_video_url.split("=")[1]
-        
-        transcript_text=YouTubeTranscriptApi.get_transcript(video_id)
-
+        video_id = youtube_video_url.split("=")[1]
+        transcript_text = YouTubeTranscriptApi.get_transcript(video_id)
         transcript = ""
         for i in transcript_text:
             transcript += " " + i["text"]
 
         return transcript
-
+        
     except Exception as e:
         raise e
-    
+
+
 ## getting the summary based on Prompt from Google Gemini Pro
 def generate_gemini_content(transcript_text,prompt):
 
-    model=genai.GenerativeModel("gemini-pro")
-    response=model.generate_content(prompt+transcript_text)
+    model = genai.GenerativeModel("gemini-pro")
+    response =  model.generate_content(prompt + transcript_text)
     return response.text
 
-st.title("YouTube Transcript to Detailed Notes Converter")
+st.title("TubeNote 📝")
 youtube_link = st.text_input("Enter YouTube Video Link:")
 
 if youtube_link:
@@ -52,7 +52,3 @@ if st.button("Get Detailed Notes"):
         summary=generate_gemini_content(transcript_text,prompt)
         st.markdown("## Detailed Notes:")
         st.write(summary)
-
-
-
-
